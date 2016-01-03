9a0c5320667d79880efaef1a8f9faf54cffb368f
Tkkg1994 <luca.grifo@outlook.com>
2016-01-03 22:32:38 +0100

    CPUfreq : Add Ironactive Governor
    this is a interactive base cpu governor, which is actually made by @Neobuddy89, well
    he knows nothing about that. So let's explain it: Samsung made some changes to the stock
    interactive governor, so if I now try to update to CAF, this wont work at all. Neobuddy
    already made so much changes to his "stock" interactive governor, it could be an own one!
    So I renamed this governor, that I can keep interactie and thisone as well. Hope nobody minds about this project
    I'm doing here.
    
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -444,6 +444,9 @@ extern struct cpufreq_governor cpufreq_gov_elementalx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
+extern struct cpufreq_governor cpufreq_gov_ironactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 #endif
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
