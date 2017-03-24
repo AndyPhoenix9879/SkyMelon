09d837be996d26a4876a5c67248784149b205c89
AndyPhoenix9879 <Lai9879M@gmail.com>
2017-03-23 19:59:46 -0700

    cpufreq.h: Cherry-pick fixup which somehow didn't caused trouble last time

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -444,7 +444,6 @@ extern struct cpufreq_governor cpufreq_gov_elementalx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
->>>>>>> 5ce32a8... cpufreq: Add NIGHTMARE Governor
 #endif
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
