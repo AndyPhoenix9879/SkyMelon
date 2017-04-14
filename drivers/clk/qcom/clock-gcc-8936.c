dc63ee08a882bbf3442e25cb568f00e8f171e94c
AndyPhoenix9879 <Lai9879M@gmail.com>
2017-04-14 10:44:46 +0800

    GPU: gfx3d for 650MHz Sure, 575MHz for 3D rendering is excellent already, but let's bump that up a bit to 650MHz

--- drivers/clk/qcom/clock-gcc-8936.c
+++ drivers/clk/qcom/clock-gcc-8936.c
@@ -844,7 +844,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 550000000,      gpll3_out_main,   2,    0,    0),
 	F( 575000000,      gpll2_gfx3d,   2,    0,    0),
 	F( 600000000,      gpll3_out_main,   2,   0,    0),
-	F( 650000000,      gpll3_out_main,   2,    0,    0),
+	F( 650000000,      gpll2_gfx3d,   2,    0,    0),
 	F( 700000000, 	   gpll3_out_main,   2,    0,    0),
 	F_END
 };
